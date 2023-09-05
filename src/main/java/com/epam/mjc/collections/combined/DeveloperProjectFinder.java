e7323bc3d0405e515ae674abfe27b3703a16cd62
Phobos027 <shkuro.stepan@gmail.com>
2023-09-05 23:45:05 +0300

    done

--- src/main/java/com/epam/mjc/collections/combined/DeveloperProjectFinder.java
+++ src/main/java/com/epam/mjc/collections/combined/DeveloperProjectFinder.java
@@ -1,10 +1,25 @@
 package com.epam.mjc.collections.combined;
 
-import java.util.List;
-import java.util.Map;
-import java.util.Set;
+import java.util.*;
 
-public class DeveloperProjectFinder {
+public class DeveloperProjectFinder{
     public List<String> findDeveloperProject(Map<String, Set<String>> projects, String developer) {
+        List <String> projectsOfDeveloper = new ArrayList<>();
+        for (Map.Entry<String, Set<String>> entry : projects.entrySet()) {
+            Set<String> developersOfProjects = entry.getValue();
+            for (String developerOfProject : developersOfProjects) {
+                if (developerOfProject.equals(developer)) projectsOfDeveloper.add(entry.getKey());
+            }
+        }
+        projectsOfDeveloper.sort(new Comparator<String>() {
+            @Override
+            public int compare(String o1, String o2) {
+                if (o1.length() != o2.length()) return o2.length() - o1.length();
+                else return o2.compareTo(o1);
+            }
+        });
+        return projectsOfDeveloper;
     }
+
+
 }
--- src/main/java/com/epam/mjc/collections/combined/MapFromKeysCreator.java
+++ src/main/java/com/epam/mjc/collections/combined/MapFromKeysCreator.java
@@ -1,9 +1,20 @@
 package com.epam.mjc.collections.combined;
 
-import java.util.Map;
-import java.util.Set;
+import java.util.*;
 
 public class MapFromKeysCreator {
     public Map<Integer, Set<String>> createMap(Map<String, Integer> sourceMap) {
+        Map<Integer, Set<String>> lengthOfKeys = new HashMap<>();
+        Set <String> values = sourceMap.keySet();
+        for (String value : values) {
+            Set <String> number = new HashSet<>();
+            for (String s : values) {
+                if (s.length() == value.length()) number.add(s);
+            }
+            lengthOfKeys.put(value.length(), number);
+        }
+        return lengthOfKeys;
+        }
+
     }
-}
+
